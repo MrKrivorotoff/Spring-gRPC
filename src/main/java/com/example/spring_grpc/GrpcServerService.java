de6f05d568b7c33748332ce9ab7ee304add89005
Dmitrii Petrov <mrkrivorotoff@gmail.com>
2026-01-07 23:10:14 +0000

    Add service impl

--- src/main/java/com/example/spring_grpc/GrpcServerService.java
+++ src/main/java/com/example/spring_grpc/GrpcServerService.java
@@ -1,6 +1,8 @@
 package com.example.spring_grpc;
 
 import io.grpc.stub.StreamObserver;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.grpc.sample.proto.HelloReply;
 import org.springframework.grpc.sample.proto.HelloRequest;
 import org.springframework.grpc.sample.proto.SimpleGrpc;
@@ -8,13 +10,38 @@ import org.springframework.stereotype.Service;
 
 @Service
 public final class GrpcServerService extends SimpleGrpc.SimpleImplBase {
+    private static final Log log = LogFactory.getLog(GrpcServerService.class);
+
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        super.sayHello(request, responseObserver);
+        log.info("Hello " + request.getName());
+        if (request.getName().startsWith("error")) {
+            throw new IllegalArgumentException("Bad name: " + request.getName());
+        }
+        if (request.getName().startsWith("internal")) {
+            throw new RuntimeException();
+        }
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + request.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void streamHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        super.streamHello(request, responseObserver);
+        log.info("Hello " + request.getName());
+        int count = 0;
+        while (count < 10) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello(" + count + ") ==> " + request.getName()).build();
+            responseObserver.onNext(reply);
+            count++;
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                responseObserver.onError(e);
+                return;
+            }
+        }
+        responseObserver.onCompleted();
     }
 }
